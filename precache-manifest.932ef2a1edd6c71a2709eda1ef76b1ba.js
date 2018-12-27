3e6b783c92c22baf00d92a1ead41d26576323669
sydinh <sydinh.dev@gmail.com>
2018-12-27 10:29:04 +0700

    Updates

---
+++ precache-manifest.932ef2a1edd6c71a2709eda1ef76b1ba.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "a80c4209af9fa8e83ec3",
+    "url": "/my-portfolio-2019/static/js/runtime~main.a80c4209.js"
+  },
+  {
+    "revision": "6ce8de1bb00b2bdbdc07",
+    "url": "/my-portfolio-2019/static/js/0.6ce8de1b.chunk.js"
+  },
+  {
+    "revision": "45ea5345c4aa76cc51a2ef562577115a",
+    "url": "/my-portfolio-2019/static/media/linkedin.45ea5345.svg"
+  },
+  {
+    "revision": "7aff79c05f8ca87bdfe5",
+    "url": "/my-portfolio-2019/static/js/main.7aff79c0.chunk.js"
+  },
+  {
+    "revision": "8a45daea32ea9f2c670e",
+    "url": "/my-portfolio-2019/static/js/3.8a45daea.chunk.js"
+  },
+  {
+    "revision": "132cf779ee54d94a32d5",
+    "url": "/my-portfolio-2019/static/js/4.132cf779.chunk.js"
+  },
+  {
+    "revision": "db9201b5546e34db8ab7",
+    "url": "/my-portfolio-2019/static/js/5.db9201b5.chunk.js"
+  },
+  {
+    "revision": "e4a1eb860f885dec90a2",
+    "url": "/my-portfolio-2019/static/js/6.e4a1eb86.chunk.js"
+  },
+  {
+    "revision": "d85de03a7c3fca4820f1",
+    "url": "/my-portfolio-2019/static/js/7.d85de03a.chunk.js"
+  },
+  {
+    "revision": "125c1097505faa4f44075364ed4aa53f",
+    "url": "/my-portfolio-2019/static/media/facebook.125c1097.svg"
+  },
+  {
+    "revision": "0114910a3602a57466d3",
+    "url": "/my-portfolio-2019/static/js/8.0114910a.chunk.js"
+  },
+  {
+    "revision": "3809adc1105553bfadca",
+    "url": "/my-portfolio-2019/static/js/1.3809adc1.chunk.js"
+  },
+  {
+    "revision": "00051dbb5b9c7a7f4dfdf04f1b633445",
+    "url": "/my-portfolio-2019/static/media/background.00051dbb.jpg"
+  },
+  {
+    "revision": "a75a5f095bf3a1155c66d012b647ca4c",
+    "url": "/my-portfolio-2019/static/media/tail-spin.a75a5f09.svg"
+  },
+  {
+    "revision": "eb19776728ded65b3a50d118f59f71cc",
+    "url": "/my-portfolio-2019/static/media/portrait.eb197767.jpg"
+  },
+  {
+    "revision": "e62ab6580976cb4e0ce4f97a9b4e4928",
+    "url": "/my-portfolio-2019/static/media/github.e62ab658.svg"
+  },
+  {
+    "revision": "5a100b46aa5dcadfabc907a76cf78c87",
+    "url": "/my-portfolio-2019/static/media/project.5a100b46.jpg"
+  },
+  {
+    "revision": "6d7c864ee430213da218a89fb0a98938",
+    "url": "/my-portfolio-2019/static/media/eye.6d7c864e.svg"
+  },
+  {
+    "revision": "70fc32d493a992fd71758da83fdd7e83",
+    "url": "/my-portfolio-2019/static/media/twitter.70fc32d4.svg"
+  },
+  {
+    "revision": "7aff79c05f8ca87bdfe5",
+    "url": "/my-portfolio-2019/static/css/main.cb160e45.chunk.css"
+  },
+  {
+    "revision": "0114910a3602a57466d3",
+    "url": "/my-portfolio-2019/static/css/8.e4cfe312.chunk.css"
+  },
+  {
+    "revision": "4ed952490fa8a8325fbea819cc8bb050",
+    "url": "/my-portfolio-2019/index.html"
+  }
+];
