29b5db09534d51e4d14a822d18b06cd6ce5338d7
sydinh <sydinh.dev@gmail.com>
2019-01-03 15:29:03 +0700

    Update condition of page routers

--- src/App.js
+++ src/App.js
@@ -1,32 +1,6 @@
-import React, { Component } from 'react';
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import React from 'react';
+import Router from 'Router';
 
-import Home from 'pages/Home';
-import About from 'pages/About';
-import Work from 'pages/Work';
-import Contact from 'pages/Contact';
-import NotFound from 'pages/NotFound';
-
-import * as routes from 'constants/routes';
-
-class App extends Component {
-  componentDidMount() {
-    document.body.classList.remove('menu-active');
-  }
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path={routes.HOME} component={Home} />
-          <Route path={routes.ABOUT} component={About} />
-          <Route path={routes.WORK} component={Work} />
-          <Route path={routes.CONTACT} component={Contact} />
-          <Route component={NotFound} />
-        </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => Router;
 
 export default App;
