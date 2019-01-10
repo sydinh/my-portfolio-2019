d1e5057962589e0da024d3779231c5d1d6502aff
sydinh <sydinh.dev@gmail.com>
2019-01-10 13:28:22 +0700

    Add work data

--- src/pages/Work/components/mockAPI.js
+++ src/pages/Work/components/mockAPI.js
@@ -1,13 +1,18 @@
 const data = [
   {
-    id: 1,
+    id: 0,
     live: 'https://frontend-styleguide.firebaseapp.com/',
     source: 'https://github.com/sydinh/frontend-styleguide',
   },
   {
-    id: 2,
+    id: 1,
     live: 'https://my-first-pwa-90f03.firebaseapp.com/',
     source: 'https://github.com/sydinh/my-first-pwa',
+  },
+  {
+    id: 2,
+    live: 'https://cra-redux-starter-kit.firebaseapp.com/',
+    source: 'https://github.com/sydinh/cra-redux-starter-kit',
   },
 ];
 
